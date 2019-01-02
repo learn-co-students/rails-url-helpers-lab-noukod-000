7e8b7e3476f827e02adda956f8a242ae8319968a
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-02 19:30:34 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,27 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    redirect_to show
+  end
+
+  def to_s
+    first_name+" "+ last_name
   end
 
   private
