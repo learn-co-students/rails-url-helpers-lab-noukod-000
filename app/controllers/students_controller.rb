596ca196784089fadd1bca48ae5d9f1a59010f63
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-26 12:36:41 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,15 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    # he will be active
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+    # without this redirection you will go to active.html.erb
+  end
+
   private
 
     def set_student
